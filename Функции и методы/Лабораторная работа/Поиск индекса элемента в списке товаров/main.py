e915900ba343de5a62fa744d2ac2df08c7b4912e
Алексей <akarpushev@yandex.ru>
2023-11-17 00:15:11 +0300

    Поиск элемента в списке товаров

--- Функции и методы/Лабораторная работа/Поиск индекса элемента в списке товаров/main.py
+++ Функции и методы/Лабораторная работа/Поиск индекса элемента в списке товаров/main.py
@@ -1,11 +1,17 @@
 # TODO Напишите функцию для поиска индекса товара
 
-
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
-def find_item(item_list, )
-    retern
+def func(a, b):
+    if b in a:
+        index_item = items_list.index(find_item)
+    else:
+        index_item = None
+    return index_item
+
+
+
 for find_item in ['банан', 'груша', 'персик']:
-    index_item = find_item
+    index_item = func(items_list, find_item)
     ...  # TODO Вызовите функцию, что получить индекс товара
     if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
