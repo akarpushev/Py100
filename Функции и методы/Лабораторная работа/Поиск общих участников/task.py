ec6737bf4ca82ce71c1a66f92bc30bb9e11ba185
Алексей <akarpushev@yandex.ru>
2023-11-17 01:15:51 +0300

    Поиск общих участников

--- Функции и методы/Лабораторная работа/Поиск общих участников/task.py
+++ Функции и методы/Лабораторная работа/Поиск общих участников/task.py
@@ -1,7 +1,15 @@
 # TODO Напишите функцию find_common_participants
 
+def func (a, b):
+    x = set(a.split('|'))
+    y = b.split('|')
+    list = sorted(x.intersection(y))
+    return list
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
+
+list = func(participants_first_group, participants_second_group)
+print(list)
